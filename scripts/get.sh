565d5eacab7a7a5c876da01e504ca0a0ac89e79f
Alper Özışık <alper.ozisik@smartface.io>
2017-11-16 12:30:57 +0000

    make run.sh to work

--- scripts/get.sh
+++ scripts/get.sh
@@ -6,9 +6,8 @@ if [ -d $folder ]; then rm -rf $folder; fi
 (
     cd ~/workspace/assets
     wget -O amcharts.zip $url
-    unzip amcharts.zip  -d amcharts
+    unzip -q amcharts.zip 'amcharts/*' -d amcharts
     rm ./amcharts.zip
     cd amcharts
-    find -type f -not -name 'amcharts' -delete
-    mv $cwd/index.html ./index.html
+    if [ -d $cwd/index.html ]; then mv $cwd/index.html ./index.html; fi
 )
