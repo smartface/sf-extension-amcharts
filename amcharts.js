19d55fc7922d5c0ee5de3b6b694b0fc049586128
Alper Özışık <alper.ozisik@smartface.io>
2017-11-17 08:19:30 +0000

    developed first runnig version

--- amcharts.js
+++ amcharts.js
@@ -1,26 +1,19 @@
 const WebView = require("sf-core/ui/webview");
 const File = require('sf-core/io/file');
 var EventEmitter = require('wolfy87-eventemitter');
+const extend = require("js-base/core/extend");
 
-
-function AMCharts(params) {
-    var ee = new EventEmitter();
-    Object.assign(this, ee);
+const AMCharts = new extend(EventEmitter)(function AMCharts(_super, params) {
+    _super(this);
     const me = this;
     params = params || {};
     var webView = params.webView || new WebView();
     var amChartsPath = "assets://amcharts/index.html";
     var chartData;
     var amChartScriptNames = [];
+    var ready = false;
+    var readyWaiting = [];
     Object.defineProperties(this, {
-        /**
-         * The directory that contains jet files. By default it is "assets://jet/index.html".
-         * 
-         * @property {String} [ jetPath = "assets://jet/index.html" ]
-         * @android
-         * @ios
-         * @since 1.0.0
-         */
         "amChartsPath": {
             get: function() {
                 return amChartsPath;
@@ -30,39 +23,14 @@ function AMCharts(params) {
             },
             enumerable: true
         },
-        /**
-         * The WebView for load and show Oracle Jet Charts inside of it. It should be provided via constructor. 
-         * If not given in constructor, webview will generated automatically.
-         * 
-         * @property {UI.WebView} webView
-         * @android
-         * @ios
-         * @since 1.0.0
-         */
         "webView": {
-            get: function() {
-                return webView;
-            },
-            set: function(value) {
-                webView = value;
-            },
+            value: webView,
+            writable: false,
             enumerable: true
         },
-        /**
-         * Refresh WebView and reload JET charts.
-         * 
-         * @method refresh
-         * @android
-         * @ios
-         * @since 1.0.0
-         */
         "refresh": {
             value: function() {
                 webView.loadFile(new File({ path: amChartsPath }));
-                webView.onShow = function(event) {
-                    var myScript = "window.drawChart('" + JSON.stringify(chartData) + "');";
-                    webView.evaluateJS(myScript, function() {});
-                };
             },
             enumerable: true
         },
@@ -77,10 +45,20 @@ function AMCharts(params) {
         },
         "loadScritsByName": {
             enumerable: true,
+            writable: false,
             value: loadScritsByName
+        },
+        "ready": {
+            enumerable: true,
+            writable: false,
+            value: readyFunction
+        },
+        "evaluateJS": {
+            enumerable: true,
+            writable: false,
+            value: webView.evaluateJS.bind(webView)
         }
     });
-    
 
     function loadScritsByName() {
         var scriptNames = [].concat(
@@ -88,17 +66,20 @@ function AMCharts(params) {
             arguments[0] :
             arguments
         );
+        scriptNames = scriptNames.filter((item) => { return amChartScriptNames.indexOf(item) === -1; });
         return new Promise((resolve, reject) => {
-            if(scriptNames.length === 0) {
+            if (scriptNames.length === 0) {
                 reject();
                 return;
             }
             var myScript = "window.addScript('" + JSON.stringify(scriptNames) + "');";
-            webView.evaluateJS(myScript, function() {
-                me.once("scriptLoaded", (data) => {
-                    resolve(data);
-                });
+            me.once("scriptLoaded", (scriptLoadStatus) => {
+                for (let s in scriptLoadStatus) {
+                    scriptLoadStatus[s] && amChartScriptNames.push(s);
+                }
+                resolve(scriptLoadStatus);
             });
+            webView.evaluateJS(myScript);
         });
 
     }
@@ -111,6 +92,7 @@ function AMCharts(params) {
             }
             catch (ex) {}
             if (eventObject.event) {
+                // console.log(`Event captured: ${eventObject.event} - ${JSON.stringify(eventObject.data)}`);
                 me.emit(eventObject.event, eventObject.data);
             }
             return false;
@@ -118,10 +100,32 @@ function AMCharts(params) {
         return true;
     };
 
+    webView.onLoad = function(event) {
+        me.once("window.onload", () => {
+            ready = true;
+            readyWaiting.forEach(item => {
+                item();
+            });
+            readyWaiting.length = 0;
+        });
+    };
+
+    function readyFunction() {
+        return new Promise((resolve, reject) => {
+            if (ready)
+                resolve();
+            else
+                readyWaiting.push(() => {
+                    resolve();
+                });
+        });
+    }
+    
+    webView.bounceEnabled = false;
 
     // after constructing
     this.refresh();
-}
+});
 
 
 module.exports = AMCharts;
--- sample/pgChart.js
+++ sample/pgChart.js
@@ -3,6 +3,10 @@ const Page = require('sf-core/ui/page');
 const WebView = require('sf-core/ui/webview');
 const FlexLayout = require('sf-core/ui/flexlayout');
 const AMCharts = require("sf-extension-amcharts");
+const Color = require('sf-core/ui/color');
+const ActivityIndicator = require('sf-core/ui/activityindicator');
+const Application = require("sf-core/application");
+
 var pgChart = new extend(Page)(
     function(_super, params) {
         var page = this;
@@ -14,27 +18,140 @@ var pgChart = new extend(Page)(
             },
             onLoad: function() {
                 var amCharts = new AMCharts({
-                    webView: page.children.wvChart
+                    webView: wvChart
                 });
-                amCharts.loadScritsByName("serial").then((status) => {
-                    console.log(`load status: ${JSON.stringify(status)}`);
-                }).catch(err => {
-                    console.log(`error: ${JSON.stringify(err)}`);
+                amCharts.ready().then(() => {
+                    amCharts.loadScritsByName("serial").then((status) => {
+                        amCharts.evaluateJS(chartScript, function() {
+                            wvChart.visible = true;
+                            aiWait.visible = false;
+                        });
+                    }).catch(e => {
+                        Application.onUnhandledError && Application.onUnhandledError(e);
+                    });
+
                 });
             }
         });
 
         page.children = page.children || {};
 
-        const wvChart = page.children.wvChart = new WebView({
+
+        const flChart = page.children.flChart = new FlexLayout({
+            width: NaN,
+            height: NaN,
+            positionType: FlexLayout.PositionType.RELATIVE,
+            visible: true,
+            backgroundColor: Color.WHITE,
+            flexGrow: 1,
+            alignItems: FlexLayout.AlignItems.CENTER,
+            justifyContent: FlexLayout.JustifyContent.CENTER
+        });
+        page.layout.addChild(flChart);
+        flChart.children = flChart.children || {};
+
+        const wvChart = flChart.children.wvChart = new WebView({
             left: 10,
             top: 10,
             right: 10,
             bottom: 10,
-            positionType: FlexLayout.PositionType.ABSOLUTE
+            positionType: FlexLayout.PositionType.ABSOLUTE,
+            visible: false
         });
-        page.layout.addChild(wvChart);
+        flChart.addChild(wvChart);
+
+        const aiWait = flChart.children.aiWait = new ActivityIndicator({
+            positionType: FlexLayout.PositionType.RELATIVE,
+            visible: true,
+            backgroundColor: Color.TRANSPARENT,
+            width: 42,
+            height: 42,
+            color: Color.BLUE
+        });
+        flChart.addChild(aiWait);
+
+        page.orientation = Page.Orientation.AUTO;
     }
 );
+
+
+const chartScript = `
+AmCharts.makeChart("chartdiv", {
+    "type": "serial",
+    "categoryField": "category",
+    "startDuration": 1,
+    "categoryAxis": {
+        "gridPosition": "start"
+    },
+    "trendLines": [],
+    "graphs": [{
+            "balloonText": "[[title]] of [[category]]:[[value]]",
+            "bullet": "round",
+            "id": "AmGraph-1",
+            "title": "graph 1",
+            "valueField": "column-1"
+        },
+        {
+            "balloonText": "[[title]] of [[category]]:[[value]]",
+            "bullet": "square",
+            "id": "AmGraph-2",
+            "title": "graph 2",
+            "valueField": "column-2"
+        }
+    ],
+    "guides": [],
+    "valueAxes": [{
+        "id": "ValueAxis-1",
+        "title": "Axis title"
+    }],
+    "allLabels": [],
+    "balloon": {},
+    "legend": {
+        "enabled": true,
+        "useGraphSettings": true
+    },
+    "titles": [{
+        "id": "Title-1",
+        "size": 15,
+        "text": "Chart Title"
+    }],
+    "dataProvider": [{
+            "category": "category 1",
+            "column-1": 8,
+            "column-2": 5
+        },
+        {
+            "category": "category 2",
+            "column-1": 6,
+            "column-2": 7
+        },
+        {
+            "category": "category 3",
+            "column-1": 2,
+            "column-2": 3
+        },
+        {
+            "category": "category 4",
+            "column-1": 1,
+            "column-2": 3
+        },
+        {
+            "category": "category 5",
+            "column-1": 2,
+            "column-2": 1
+        },
+        {
+            "category": "category 6",
+            "column-1": 3,
+            "column-2": 2
+        },
+        {
+            "category": "category 7",
+            "column-1": 6,
+            "column-2": 8
+        }
+    ]
+});`;
+
 
 module.exports = exports = pgChart;
